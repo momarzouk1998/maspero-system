f2ef6a7fb2c3bc1956baabe516156de0c95f2705
sohbatalquran411 <sohbatalquran411@gmail.com>
2026-08-05 01:23:16 +0300

    config: add output standalone for docker build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
+
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb'
